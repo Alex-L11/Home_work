8377a7e805e33a32d5668aa36214149b17727736
Alex <hopiru@mail.ru>
2026-08-25 22:45:28 +0300

    добавлен тест функции get_mask_account

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,4 +1,4 @@
-from src.masks import get_mask_card_number
+from src.masks import get_mask_card_number, get_mask_account
 import pytest
 
 def test_get_mask_card_number():
@@ -15,6 +15,19 @@ def test_get_mask_card_number():
 
     with pytest.raises(TypeError):
         get_mask_card_number('qwerrtyty')
+
+
+def test_get_mask_account():
+    assert get_mask_account(73654108430135874305) == '** 4305'
+
+    with pytest.raises(TypeError):
+        get_mask_account(3445666778778)
+
+    with pytest.raises(TypeError):
+        get_mask_account(324376677345676788888454654)
+
+    with pytest.raises(NameError):
+        get_mask_account(asfdf)
 
 
 
