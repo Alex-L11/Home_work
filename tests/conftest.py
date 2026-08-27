77fbd77af9194a100befd1e507c30a6aead921bb
Alex <hopiru@mail.ru>
2026-08-27 16:07:35 +0300

    добавлены фикстуры для проверки функции sort_by_date

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,6 +30,21 @@ def dict_list_2():
             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
             ]
 
+@pytest.fixture
+def dict_list_2_a():
+    return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            ]
+@pytest.fixture
+def dict_list_2_b():
+    return [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+            ]
+
 
 @pytest.fixture
 def dict_list_3():
@@ -58,3 +73,21 @@ def dict_list_4_a():
 @pytest.fixture
 def dict_list_5():
     return [{'id': 4432232}]
+
+
+@pytest.fixture
+def dict_list_date():
+    return [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            ]
+
+
+@pytest.fixture
+def dict_list_date_reverse():
+    return [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-06-30T02:08:58.425572'},
+            {'id': 41428829, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+            ]
