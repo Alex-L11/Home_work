06b5eeb33e3e75893d4b172f962b0b3b58f40a4b
Alex <hopiru@mail.ru>
2026-09-03 17:00:41 +0300

    добавлены тесты к модулю decorators

---
+++ tests/test_decorators.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+from typing import Any
+
+import pytest
+
+from src.decorators import log
+
+
+def test_log(capsys: Any) -> None:
+    @log()
+    def wrap(x, y):
+        return x + y
+
+    result = wrap(2, 4)
+    assert result == 6
+
+    out, err = capsys.readouterr()
+    assert "wrap ok" in err
+    assert out == ""
+
+
+def test_log_file():
+    log_file = "mylog.txt"
+    if os.path.exists(log_file):
+        os.remove(log_file)
+
+    @log(filename=log_file)
+    def wrap(x, y):
+        return x + y
+
+    result = wrap(2, 5)
+    assert result == 7
+
+    # проверяем что файл создан
+    assert Path(log_file).exists()
+
+    # читаем файл и проверяем содержимое
+    content = Path(log_file).read_text()
+    assert "wrap ok" in content
+
+    with pytest.raises(TypeError):
+        wrap(1, "1")
