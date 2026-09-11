e72dcd16b2393775234d36850ee983b2dc7d41cc
Alex <hopiru@mail.ru>
2026-09-11 13:13:22 +0300

    добавлены тесты для модуля utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,8 @@
+import pytest
 import json
 from unittest.mock import mock_open, patch
 
-from src.utils import get_list_transactions
+from src.utils import get_list_transactions, get_transaction
 
 
 def test_get_list_transactions() -> None:
@@ -48,3 +49,26 @@ def test_get_list_transactions_not_a_list() -> None:
     with patch("builtins.open", mock_open(read_data=json_str)):
         result = get_list_transactions(("mock"))
         assert result == []
+
+
+@patch("src.utils.json.load")
+def test_get_list_transactions_no_json_format(mock_load, capsys) -> None:
+    mock_load.side_effect = json.JSONDecodeError("Ошибка декодирования файла", "", 0)
+    result = get_list_transactions("mock")
+    assert result == []
+
+
+def test_get_transactions(dict_transaction) -> float:
+    assert get_transaction(dict_transaction) == 31957.58
+
+
+def test_get_transactions_usd(dict_transaction_usd) -> float:
+    assert get_transaction(dict_transaction_usd) == 2690818.01
+
+
+def test_get_transactions_none() -> float:
+    assert get_transaction({}) == 0.0
+
+
+def test_get_transactions_amount_str(dict_trans_1) -> None:
+   assert get_transaction(dict_trans_1) == 0.0
