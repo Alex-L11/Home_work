2108d52317c571aee5c2964a388e774834803b1d
Alex <hopiru@mail.ru>
2026-09-11 10:53:03 +0300

    добалена функция get_list_transactions

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
-import os
 import json
-
+import os
 from typing import Any
 
+from src.external_api import currency_converter
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 def get_list_transactions(way_json: str) -> list[dict[str, Any]]:
     """Принимает путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
@@ -23,5 +25,29 @@ def get_list_transactions(way_json: str) -> list[dict[str, Any]]:
         return []
 
 
+def get_transaction(data_trans: dict[str, Any]) -> float:
+    """Принимает транзакцию и возвращает сумму транзакции в рублях."""
+    get_amount = data_trans.get("operationAmount", {})
+    get_currency = get_amount.get("currency", {})
+    amount_transaction = get_amount.get("amount")
 
+    if isinstance(data_trans, dict):
+        currency = get_currency.get("code", "").upper()
+    else:
+        currency = str(get_currency).upper()
 
+    if not amount_transaction:
+        return 0.0
+
+    try:
+        amount = float(amount_transaction)
+    except ValueError, TypeError:
+        return 0.0
+
+    if currency == "RUB":
+        return amount
+
+    if currency in ("EUR", "USD"):
+        return currency_converter(currency, amount)
+
+    return currency_converter(currency, amount)
