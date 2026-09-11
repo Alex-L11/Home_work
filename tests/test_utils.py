57d8dfe29f9df5d22e74a99ab0e08fd3276c4234
Alex <hopiru@mail.ru>
2026-09-11 10:54:18 +0300

    тесты приведены согласно PEP8

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,36 +1,27 @@
 import json
-from unittest.mock import patch, mock_open
+from unittest.mock import mock_open, patch
+
 from src.utils import get_list_transactions
 
-def test_get_list_transactions():
-    mock_transactions = [{
-    "id": 441945886,
-    "state": "EXECUTED",
-    "date": "2019-08-26T10:50:58.294041",
-    "operationAmount": {
-      "amount": "31957.58",
-      "currency": {
-        "name": "руб.",
-        "code": "RUB"
-      }
-    },
-    "description": "Перевод организации",
-    "from": "Maestro 1596837868705199",
-    "to": "Счет 64686473678894779589"
-  },
-  {
-    "id": 41428829,
-    "state": "EXECUTED",
-    "date": "2019-07-03T18:35:29.512364",
-    "operationAmount": {
-      "amount": "8221.37",
-      "currency": {
-        "name": "USD",
-        "code": "USD"
-      }
-    }
-  }
-]
+
+def test_get_list_transactions() -> None:
+    mock_transactions = [
+        {
+            "id": 441945886,
+            "state": "EXECUTED",
+            "date": "2019-08-26T10:50:58.294041",
+            "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Maestro 1596837868705199",
+            "to": "Счет 64686473678894779589",
+        },
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+            "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
+        },
+    ]
     json_str = json.dumps(mock_transactions)
     with patch("builtins.open", mock_open(read_data=json_str)) as m_open:
         result = get_list_transactions("mock/path")
@@ -38,24 +29,22 @@ def test_get_list_transactions():
         m_open.assert_called_once()
 
 
-def test_get_list_transactions_empty_file():
+def test_get_list_transactions_empty_file() -> None:
     json_str = json.dumps([])
     with patch("builtins.open", mock_open(read_data=json_str)) as m_open:
-        result = get_list_transactions(("mock/path"))
+        result = get_list_transactions(("mock"))
         assert result == []
         m_open.assert_called_once()
 
 
-def test_get_list_transactions_file_not_found():
+def test_get_list_transactions_file_not_found() -> None:
     with patch("builtins.open", side_effect=FileNotFoundError("Файд не найден")):
-        result = get_list_transactions(("mock/path"))
+        result = get_list_transactions(("mock"))
         assert result == []
 
 
-def test_get_list_transactions_not_a_list():
+def test_get_list_transactions_not_a_list() -> None:
     json_str = json.dumps({})
     with patch("builtins.open", mock_open(read_data=json_str)):
-        result = get_list_transactions(("mock/path"))
+        result = get_list_transactions(("mock"))
         assert result == []
-
-
