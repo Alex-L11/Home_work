2c551506027377a8d03db736069b0f17c3a6b9ed
Alex <hopiru@mail.ru>
2026-09-15 18:30:40 +0300

    изменен имя фикстуры для модуля reading_fin_trans, универсальное название для 2-х функций в модуле

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from typing import Any, List, Hashable
+from typing import Any, List
 
 import pytest
 
@@ -347,16 +347,28 @@ def dict_trans_2() -> dict:
 
 
 @pytest.fixture
-def trans_csv_1() -> list[dict[str, Any]]:
+def trans_1() -> list[dict[str, Any]]:
     return [
         {
-            'id': 650703.0, 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': 16210.0, 'currency_name': 'Sol',
-         'currency_code': 'PEN', 'from': 'Счет 58803664561298323391', 'to': 'Счет 39745660563456619397',
-         'description': 'Перевод организации'
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
         },
         {
-            'id': 3598919.0, 'state': 'EXECUTED', 'date': '2020-12-06T23:00:58Z',
-         'amount': 29740.0, 'currency_name': 'Peso', 'currency_code': 'COP', 'from': 'Discover 3172601889670065',
-         'to': 'Discover 0720428384694643', 'description': 'Перевод с карты на карту'
-        }
+            "id": 3598919.0,
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": 29740.0,
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту",
+        },
     ]
