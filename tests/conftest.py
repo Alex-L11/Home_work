a5b12b8a03c79a63a0ef51d58b24540b87337a51
Alex <hopiru@mail.ru>
2026-09-21 21:25:44 +0300

    добавлена фикстура для теста функции format_transactions

--- tests/conftest.py
+++ tests/conftest.py
@@ -339,8 +339,51 @@ def dict_trans_2() -> dict:
         "id": 441945886,
         "state": "EXECUTED",
         "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {"amount": "234434", "currency": {"name": "USD", "code": "usd"}},
+        "operationAmount": {"amount": "234434.1", "currency": {"name": "USD", "code": "usd"}},
         "description": "Перевод организации",
         "from": "Maestro 1596837868705199",
         "to": "Счет 64686473678894779589",
     }
+
+
+@pytest.fixture
+def trans_1() -> list[dict[str, Any]]:
+    return [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        },
+        {
+            "id": 3598919.0,
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": 29740.0,
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту",
+        },
+    ]
+
+
+@pytest.fixture
+def dict_for_test() -> dict:
+    return         {
+            "id": 3598919.0,
+            "state": "EXECUTED",
+            "date": "2020-12-06T23:00:58Z",
+            "amount": 29740.0,
+            "currency_name": "Peso",
+            "currency_code": "COP",
+            "from": "Discover 3172601889670065",
+            "to": "Discover 0720428384694643",
+            "description": "Перевод с карты на карту",
+        }
