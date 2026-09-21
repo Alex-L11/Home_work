d9c305d98c095ea5abf93358f3d4dcda58c5b63f
Alex <hopiru@mail.ru>
2026-09-21 12:46:25 +0300

    добвлена функция process_bank_operations для вывода категорий по названиям и их количеству

---
+++ src/process_bank.py
@@ -0,0 +1,44 @@
+import re
+
+from typing import Any
+from collections import Counter
+
+
+def process_bank_search(data: list[dict[str, Any]], search: str) -> list[dict[str, Any]]:
+    """Принимает список словарей с данными о банковских операциях и строку поиска, возвращает список словарей, у
+    которых в описании есть данная строка."""
+
+    # задаем шаблон поиска независимо от регистра
+    pattern = re.compile(re.escape(search), flags=re.IGNORECASE)
+    bank_trans = []
+    if search == "":
+        raise TypeError("Пустая строка для поиска")
+    for item in data:
+        # переводим словарь в строку
+        data_str = str(item)
+        if pattern.search(data_str):
+            bank_trans.append(item)
+
+    return bank_trans
+
+
+def process_bank_operations(data: list[dict[str, Any]], categories: list) -> dict[str, int]:
+    """Принимает список словарей с данными о банковских операциях и список категорий операций, возвращает словарь,
+    в котором ключи - это названия категорий, значения - это количетсво операций в каждой категории."""
+
+    if not categories:
+        raise TypeError("Пустой список категорий")
+    # Объединяем все категории в один паттерн через ИЛИ
+    pattern = re.compile("|".join(categories), re.IGNORECASE)
+
+    cat = []
+
+    for item in data:
+        # ищем ключ "description"
+        description = item.get("description", "")
+        # возвращаем список всех найденных значений, которые совпали с шаблоном и добавляем все в общий список
+        cat.extend(pattern.findall(description))
+    # создаем счетчик по каждой категории
+    result = Counter(cat)
+
+    return dict(result)
